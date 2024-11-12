102db85a446f805dfa3de8ea0fcff0b4da592093
GeNieSed <gogogolike@mail.ru>
2024-11-12 21:55:37 +0300

    init: vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    pages: {
+        index: {
+            entry: 'app/src/main.js',
+            template: 'public/index.html',
+            filename: 'index.html',
+        },
+    },
+};

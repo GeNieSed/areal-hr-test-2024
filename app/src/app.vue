d70addb91b0c79616856bf97eefb037f6af4a9d6
GeNieSed <gogogolike@mail.ru>
2024-11-12 21:56:58 +0300

    refactor: повышение читабельности кода

---
+++ app/src/app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <h1>Управление данными</h1>
+    <button @click="$router.push('/')">Организации</button>
+    <button @click="$router.push('/departments')">Отделы</button>
+    <button @click="$router.push('/positions')">Должности</button>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>

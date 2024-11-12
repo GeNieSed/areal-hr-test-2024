d4e686eb84a2e6a859a4ee6ead9478f06071b934
GeNieSed <gogogolike@mail.ru>
2024-11-12 21:59:39 +0300

    refactor: повышение читабельности кода

--- app/src/router.js
+++ app/src/router.js
@@ -1,18 +1,31 @@
-import {createRouter, createWebHistory} from 'vue-router';
-import readOrganizations from "../components/readOrganizations.vue";
-import addOrganizations from "../components/addOrganizations.vue";
-import editOrganizations from "../components/editOrganizations.vue";
 
+import { createRouter, createWebHistory } from 'vue-router';
+import ReadOrganizations from '../components/organizations/readOrganizations.vue';
+import AddOrganization from '../components/organizations/addOrganizations.vue';
+import EditOrganization from '../components/organizations/editOrganizations.vue';
 
 const routes = [
-    {path: '/', redirect: '/organizations'},
-    {path: '/organizations', component: readOrganizations},
-    {path: '/organizations-add', component: addOrganizations},
-    {path: '/organizations-edit/:id', component: editOrganizations, props: true},
+    {
+        path: '/',
+        name: 'Home',
+        component: ReadOrganizations,
+    },
+    {
+        path: '/organization-add',
+        name: 'AddOrganization',
+        component: AddOrganization,
+    },
+    {
+        path: '/organization-edit/:id',
+        name: 'EditOrganization',
+        component: EditOrganization,
+        props: true,
+    },
 ];
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
 });
+
 export default router;

91400e588f20f191ad5b3a7df00d911f8a2d225f
GeNieSed <gogogolike@mail.ru>
2024-11-12 21:53:35 +0300

    fix: исправление пути файла

--- api/scr/routes/routerOrganizations.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const organizationController = require('../controllers/organizationController');
-
-router.get('/organization', organizationController.readOrganizations);
-router.post('/organization', organizationController.createOrganization);
-router.put('/:id', organizationController.updateOrganization);
-router.delete('/:id', organizationController.deleteOrganization);
-module.exports = router;

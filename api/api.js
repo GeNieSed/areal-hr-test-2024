af11f7f211fb545746853c1cccfd7c25627ee170
GeNieSed <gogogolike@mail.ru>
2024-11-13 22:41:01 +0300

    feat: добавление новых routers

--- api/api.js
+++ api/api.js
@@ -3,6 +3,8 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const organizationRoutes = require('./src/routes/routerOrganizations');
+const positionsRoutes = require('./src/routes/routerPositions');
+const departmentRoutes = require('./src/routes/routerDepartments');
 require('dotenv').config();
 
 const app = express();
@@ -12,7 +14,9 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/api', organizationRoutes);
-
+app.use('/api', positionsRoutes);
+app.use('/api' , departmentRoutes)
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+

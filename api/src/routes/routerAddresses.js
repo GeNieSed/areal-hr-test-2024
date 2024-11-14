6fb333c8eb2ea76a685d67c1811ad8b6f7ba1dcd
GeNieSed <gogogolike@mail.ru>
2024-11-14 20:01:41 +0300

    feat: добавление CRUD операций

---
+++ api/src/routes/routerAddresses.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const addressesController = require('../controllers/addressesController');
+
+router.get('/addresses', addressesController.readAddresses);
+
+
+// Добавить новую организацию
+router.post('/addresses', addressesController.createAddresses);
+
+
+// Обновить существующую организацию по ID
+router.put('/addresses/:id', addressesController.updateAddresses);
+
+
+// Удалить организацию по ID
+router.delete('/addresses/:id', addressesController.deleteAddresses);
+
+module.exports = router;

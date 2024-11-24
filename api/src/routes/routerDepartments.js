759128cf662c6cdb85e3d950df0885420def3d1b
GeNieSed <gogogolike@mail.ru>
2024-11-24 18:26:19 +0300

    feat: создан get запрос по id

--- api/src/routes/routerDepartments.js
+++ api/src/routes/routerDepartments.js
@@ -2,18 +2,19 @@ const express = require('express');
 const router = express.Router();
 const departmentController = require('../controllers/departmentController');
 
+// Получить все отделы
 router.get('/department', departmentController.readDepartments);
 
+// Получить отдел по ID
+router.get('/department/:id', departmentController.readDepartmentById);
 
-// Добавить новую организацию
+// Добавить новый отдел
 router.post('/department', departmentController.createDepartment);
 
-
-// Обновить существующую организацию по ID
+// Обновить существующий отдел по ID
 router.put('/department/:id', departmentController.updateDepartment);
 
-
-// Удалить организацию по ID
+// Удалить отдел по ID
 router.delete('/department/:id', departmentController.deleteDepartment);
 
 module.exports = router;

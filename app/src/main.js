1058d3d4c921c676cc6026aed14445fbd6e81105
GeNieSed <gogogolike@mail.ru>
2024-12-08 20:07:58 +0300

    init: добавление путей

--- app/src/main.js
+++ app/src/main.js
@@ -1,5 +1,6 @@
-import { createApp } from 'vue';
-import App from './app.vue';
-import router from './router';
+import { createApp } from "vue";
+import App from "./app.vue";
+import router from "./router";
 
-createApp(App).use(router).mount('#app');
+import "@/css/styles.css";
+createApp(App).use(router).mount("#app");

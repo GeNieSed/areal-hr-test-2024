c2323000b2e5aeb7c7304da93f959f15b4349af6
GeNieSed <gogogolike@mail.ru>
2024-12-11 22:39:20 +0300

    feat: добавление путей

--- api/api.js
+++ api/api.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const organizationRoutes = require('./src/routes/routerOrganizations');
-const positionsRoutes = require('./src/routes/routerPositions');
-const departmentRoutes = require('./src/routes/routerDepartments');
+const organizationRoutes = require('./src/routes/router-organizations');
+const positionsRoutes = require('./src/routes/router-positions');
+const departmentRoutes = require('./src/routes/router-departments');
+const indexRoutes = require('./src/routes/router-index');
+const operationRoutes = require('./src/routes/router-operations');
+const typesRoutes = require('./src/routes/routes-types');
+const employeeRoutes = require('./src/routes/router-employees');
+const addressesRoutes = require('./src/routes/router-addresses');
+const passportRouter = require('./src/routes/router-passport');
+const filesRouter = require('./src/routes/router-file');
 require('dotenv').config();
 
 const app = express();
@@ -15,6 +22,14 @@ app.use(bodyParser.json());
 app.use('/api', organizationRoutes);
 app.use('/api', positionsRoutes);
 app.use('/api', departmentRoutes);
+app.use('/api', indexRoutes);
+app.use('/api', operationRoutes);
+app.use('/api', typesRoutes);
+app.use('/api', employeeRoutes);
+app.use('/api', addressesRoutes);
+app.use('/api', passportRouter);
+app.use('/api', filesRouter);
+app.use('/api', positionsRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
